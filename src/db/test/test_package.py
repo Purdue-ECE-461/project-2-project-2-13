3e0f03faa593fe8e435e67237ca99448ec25942b
Geoffrey Cramer <gcwill70@gmail.com>
2021-11-15 09:16:20 -0500

    very basic get/set in db

--- src/db/test/test_package.py
+++ src/db/test/test_package.py
@@ -1,8 +1,21 @@
+import db
 from db.test import BaseTestCase
+from openapi_server.models.package import Package, PackageMetadata, PackageData
 
 class TestDbPackage(BaseTestCase):
-    def test_get():
-        pass
+    def test_get(self):
+        expected = {'metadata': {'name': 'test', 'version': '1.2.3', 'id': id}, 'data': {'content': 'Content', 'url': 'URL', 'js_program': 'JS_Program'}}
+        db.DB['package'][id] = expected
 
-    def test_set():
-        pass
+        actual = db.get('package', id)
+
+        assert actual == expected
+
+    def test_set(self):
+        expected = {'metadata': {'name': 'test', 'version': '1.2.3', 'id': id}, 'data': {'content': 'Content', 'url': 'URL', 'js_program': 'JS_Program'}}
+        
+        db.set('package', id, expected)
+
+        actual = db.DB['package'][id]
+        
+        assert actual == expected
