293720593ffc393e3fd0bdeed4ad202b2dccd2ad
Geoffrey Cramer <gcwill70@gmail.com>
2021-11-29 21:47:35 -0500

    deploy with gunicorn instead of python CLI

--- src/openapi_server/__main__.py
+++ src/openapi_server/__main__.py
@@ -1,18 +1,6 @@
-#!/usr/bin/env python3
-
 import logging
-import connexion
+from openapi_server import app
 
-from openapi_server import encoder
-
-
-def main():
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml')
-    app.run(port=8080, debug=True)
-
-
-if __name__ == '__main__':
-    main()
+    app.run(host="localhost", port=8080, debug=True)
